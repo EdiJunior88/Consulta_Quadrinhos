4aaca943e8dee9f372f951bb3347d6c92ac1d445
EdiJunior88 <er.moura88@yahoo.com.br>
2023-01-10 14:30:32 -0300

    - Estilização via CSS do componente Busca

--- app/src/Componentes/Busca/Busca.jsx
+++ app/src/Componentes/Busca/Busca.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
+import styles from "./Busca.module.css";
 
-const Busca = ({ busca }) => {
+const Busca = ({ busca, props }) => {
   const [texto, setTexto] = useState("");
 
   const pesquisa = (evento) => {
@@ -9,17 +10,18 @@ const Busca = ({ busca }) => {
   };
 
   function inputTeclado(evento) {
-    if (evento.key === 'Enter') {
+    if (evento.key === "Enter") {
       evento.preventDefault();
     }
   }
 
   return (
-    <div>
+    <div className={styles.containerCampoBusca}>
       <form>
         <input
           type='text'
-          placeholder='Digite o nome do personagem'
+          className={styles.campoBusca}
+          placeholder="Digite o nome da comic"
           autoFocus
           onChange={(evento) => pesquisa(evento.target.value)}
           value={texto}
